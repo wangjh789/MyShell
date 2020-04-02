873e31ad458ea9758e44ebdd1e0083c2e8c81be3
jonghwi Wang <wangjh789@daum.net>
2020-04-02 16:57:08 +0900

    add pwd,cd

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+#include<unistd.h> //getcwd
+
+void mypwd(){ 
+    char buff[100];
+    getcwd(buff,100);
+    printf("%s\n",buff);
+}
+void mycd(char* path){ //filepath
+    if(chdir(path) == -1){
+        printf("failed");
+    }
+}
 
 int main(){
-    printf("test");
+    mycd("testDir");
+    mypwd();
 }
