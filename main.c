c7e3adf32c20b68b58ed7d36cdc49c9c7c4c2a58
jonghwi Wang <wangjh789@daum.net>
2020-04-02 22:56:02 +0900

    update ignore

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include<unistd.h> //getcwd
 
-void mypwd(){ 
-    char buff[100];
-    getcwd(buff,100);
-    printf("%s\n",buff);
+void mypwd(char output[]){  
+	getcwd (output, 128); 
 }
 void mycd(char* path){ //filepath
     if(chdir(path) == -1){
         printf("failed");
     }
 }
+void myls(){
+
+}
 
 int main(){
+    char output[100];
+
+    mypwd(output);
+    printf("%s\n",output);
     mycd("testDir");
-    mypwd();
+    mypwd(output);
+    printf("%s\n",output);
+
 }
